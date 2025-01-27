fbe062770e4e3db9f083a2d957879b082d532d06
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-01-27 10:30:16 -0300

    Adiciona componente Task no ToDo

--- src/components/ToDo/ToDo.jsx
+++ src/components/ToDo/ToDo.jsx
@@ -1,9 +1,19 @@
+import Task from '../Task/Task'
 import './ToDo.css'
 
 function ToDo() {
+
+  const handleClick = () =>{
+    window.alert("Clicou")
+  }
+
+  const handleCheck = () => {
+    window.alert("Check!")
+  }
+
     return (
       <>
-        ToDo funciona!
+        <Task titulo={"Task 1"} funcaoCheck={handleCheck} funcaoClick={handleClick}/>
       </>
     )
 }

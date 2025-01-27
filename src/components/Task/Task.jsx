61b28d74d7e7ce2365acdddee46ecbd0b38a683b
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-01-27 09:57:16 -0300

    Adiciona estrutura do conteúdo base para o componente task

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -2,9 +2,11 @@ import './Task.css'
 
 function Task() {
     return (
-      <>
-        Task funciona!
-      </>
+      <div>
+        <input type="checkbox" name="concluido" id="concluido-taskX" />
+        <span>Titulo da task X</span>
+        <button>🗑️</button>
+      </div>
     )
 }
 
